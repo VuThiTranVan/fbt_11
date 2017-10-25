d805bc7efebb5a66a694bde167c1a74b5b7fb37d
vuthitranvan <vu.thi.tran.van@framgia.com>
2017-10-25 15:52:05 +0700

    Login & Logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  include SessionsHelper
+
   def valid_info object
     render file: "public/404.html", status: 404, layout: false unless object
   end
